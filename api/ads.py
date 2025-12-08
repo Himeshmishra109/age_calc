86738a43a84291889ad5acee2cee101a8a944ef0
Himesh Mishra <himeshm548@gmail.com>
2025-12-09 02:10:29 +0530

    Delete api directory

--- api/ads.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, redirect
-
-app = Flask(__name__)
-
-@app.route('/ads.txt')
-def ads_txt():
-    return redirect("https://srv.adstxtmanager.com/19390/clackmasterpro.online", code=302)
-
-handler = app
